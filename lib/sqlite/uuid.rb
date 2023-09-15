eea39f937644327002340ab6d4ac973ef01f1568
clayton <6334+clayton@users.noreply.github.com>
2023-09-15 12:27:15 -0700

    initial

---
+++ lib/sqlite/uuid.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "uuid/version"
+
+module Sqlite
+  module Uuid
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sqlite/uuid/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Sqlite
+  module Uuid
+    VERSION = "0.1.0"
+  end
+end
